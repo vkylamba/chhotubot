0af7c36e7036f59f252abe07a87a1bf1ea42fc23
vikas lamba <vikas13jun@gmail.com>
2020-08-18 17:50:07 +0200

    cleanup

--- src/car.h
+++ src/car.h
@@ -2,106 +2,74 @@
 #define _CAR_H_
 #include "motor_shield/AFMotor.h"
 
+#define INITIALIZATION_SPEED 10
+#define CAR_SPEED 80
+
 class Car
 {
     public:
-    Car(AF_DCMotor motor_a, AF_DCMotor motor_b, AF_DCMotor motor_c, AF_DCMotor motor_d)
+    Car(AF_DCMotor motor_a, AF_DCMotor motor_b)
     {
 
-        motor_a.setSpeed(200);
+        motor_a.setSpeed(INITIALIZATION_SPEED);
         motor_a.run(RELEASE);
 
-        motor_b.setSpeed(200);
+        motor_b.setSpeed(INITIALIZATION_SPEED);
         motor_b.run(RELEASE);
 
-        motor_c.setSpeed(200);
-        motor_c.run(RELEASE);
-
-        motor_d.setSpeed(200);
-        motor_d.run(RELEASE);
     }
 
-    void move_forward(AF_DCMotor motor_a, AF_DCMotor motor_b, AF_DCMotor motor_c, AF_DCMotor motor_d);
-    void move_backward(AF_DCMotor motor_a, AF_DCMotor motor_b, AF_DCMotor motor_c, AF_DCMotor motor_d);
-    void move_left(AF_DCMotor motor_a, AF_DCMotor motor_b, AF_DCMotor motor_c, AF_DCMotor motor_d);
-    void move_right(AF_DCMotor motor_a, AF_DCMotor motor_b, AF_DCMotor motor_c, AF_DCMotor motor_d);
-    void stop(AF_DCMotor motor_a, AF_DCMotor motor_b, AF_DCMotor motor_c, AF_DCMotor motor_d);
+    void move_forward(AF_DCMotor motor_a, AF_DCMotor motor_b);
+    void move_backward(AF_DCMotor motor_a, AF_DCMotor motor_b);
+    void move_left(AF_DCMotor motor_a, AF_DCMotor motor_b);
+    void move_right(AF_DCMotor motor_a, AF_DCMotor motor_b);
+    void stop(AF_DCMotor motor_a, AF_DCMotor motor_b);
 };
 
-void Car::move_forward(AF_DCMotor motor_a, AF_DCMotor motor_b, AF_DCMotor motor_c, AF_DCMotor motor_d)
+void Car::move_forward(AF_DCMotor motor_a, AF_DCMotor motor_b)
 {
     motor_a.run(FORWARD);
-    motor_a.setSpeed(250);
+    motor_a.setSpeed(CAR_SPEED);
 
     motor_b.run(FORWARD);
-    motor_b.setSpeed(250);
-
-    motor_c.run(FORWARD);
-    motor_c.setSpeed(250);
-
-    motor_d.run(FORWARD);
-    motor_d.setSpeed(250);
+    motor_b.setSpeed(CAR_SPEED);
 }
 
-void Car::move_left(AF_DCMotor motor_a, AF_DCMotor motor_b, AF_DCMotor motor_c, AF_DCMotor motor_d)
+void Car::move_left(AF_DCMotor motor_a, AF_DCMotor motor_b)
 {
     motor_a.run(BACKWARD);
-    motor_a.setSpeed(250);
+    motor_a.setSpeed(CAR_SPEED);
 
     motor_b.run(BACKWARD);
-    motor_b.setSpeed(250);
-
-    motor_c.run(FORWARD);
-    motor_c.setSpeed(250);
-
-    motor_d.run(FORWARD);
-    motor_d.setSpeed(250);
+    motor_b.setSpeed(CAR_SPEED);
 }
 
-void Car::move_right(AF_DCMotor motor_a, AF_DCMotor motor_b, AF_DCMotor motor_c, AF_DCMotor motor_d)
+void Car::move_right(AF_DCMotor motor_a, AF_DCMotor motor_b)
 {
     motor_a.run(FORWARD);
-    motor_a.setSpeed(250);
+    motor_a.setSpeed(CAR_SPEED);
 
     motor_b.run(FORWARD);
-    motor_b.setSpeed(250);
-
-    motor_c.run(BACKWARD);
-    motor_c.setSpeed(250);
-
-    motor_d.run(BACKWARD);
-    motor_d.setSpeed(250);
+    motor_b.setSpeed(CAR_SPEED);
 }
 
 
-void Car::move_backward(AF_DCMotor motor_a, AF_DCMotor motor_b, AF_DCMotor motor_c, AF_DCMotor motor_d)
+void Car::move_backward(AF_DCMotor motor_a, AF_DCMotor motor_b)
 {
     motor_a.run(BACKWARD);
-    motor_a.setSpeed(250);
+    motor_a.setSpeed(CAR_SPEED);
 
     motor_b.run(BACKWARD);
-    motor_b.setSpeed(250);
-
-    motor_c.run(BACKWARD);
-    motor_c.setSpeed(250);
-
-    motor_d.run(BACKWARD);
-    motor_d.setSpeed(250);
+    motor_b.setSpeed(CAR_SPEED);
 }
 
-void Car::stop(AF_DCMotor motor_a, AF_DCMotor motor_b, AF_DCMotor motor_c, AF_DCMotor motor_d)
+void Car::stop(AF_DCMotor motor_a, AF_DCMotor motor_b)
 {
-    motor_a.setSpeed(250);
+    motor_a.setSpeed(CAR_SPEED);
     motor_a.run(RELEASE);
 
-    motor_b.setSpeed(250);
+    motor_b.setSpeed(CAR_SPEED);
     motor_b.run(RELEASE);
-
-    motor_c.setSpeed(250);
-    motor_c.run(RELEASE);
-
-    motor_d.setSpeed(250);
-    motor_d.run(RELEASE);
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -14,30 +14,51 @@
 Servo servo_h;
 Servo servo_v;
 
-#ifndef LED_BUILTIN
-#define LED_BUILTIN 13
-#endif
+#define MAX_BUFFER_LENGTH 200
+// AF_DCMotor motor1(1);
+// AF_DCMotor motor2(4);
+// AF_DCMotor motor3(2);
+// AF_DCMotor motor4(3);
 
-AF_DCMotor motor1(1);
+AF_DCMotor motor1(3);
 AF_DCMotor motor2(4);
-AF_DCMotor motor3(2);
-AF_DCMotor motor4(3);
 
 
-Car car = Car(motor1, motor2, motor3, motor4);
+Car car = Car(motor1, motor2);
 IR ir_left(0);
 IR ir_right(1);
 IR ir_front(2);
 
-char buffer[100];
-char extra[50];
-uint8_t counter = 0, buffer_counter=0;
+char buffer_in[MAX_BUFFER_LENGTH];
+char buffer_out[MAX_BUFFER_LENGTH];
+uint16_t counter = 0, buffer_counter=0;
 int forward = 0;
 int backward = 0;
 int left = 0;
 int right = 0;
-int servo_horizontal = 0;
-int servo_vertical = 0;
+int servo_horizontal = 90;
+int servo_vertical = 90;
+uint8_t indicator_led_state = 0;
+
+
+void test_movements() {
+  car.move_forward(motor1, motor2);
+  delay(2000);
+  car.stop(motor1, motor2);
+  delay(1000);
+  car.move_left(motor1, motor2);
+  delay(500);
+  car.stop(motor1, motor2);
+  delay(1000);
+  car.move_forward(motor1, motor2);
+  delay(2000);
+  car.stop(motor1, motor2);
+  delay(1000);
+  car.move_right(motor1, motor2);
+  delay(500);
+  car.stop(motor1, motor2);
+}
+
 
 void setup()
 {
@@ -48,146 +69,115 @@ void setup()
   servo_v.write(90);
   // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+
+  // test_movements();
 }
+
+
+void clear_buffer(char *buffer, uint16_t buffer_length) {
+  for(uint16_t i=0;i<buffer_length;i++) {
+    buffer[i] = '\0';
+  }
+}
+
+void toggle_led() {
+  if (indicator_led_state > 0) {
+    indicator_led_state = 1;
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    indicator_led_state = 0;
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+}
+
+void move_head(int servo_horizontal, int servo_vertical) {
+  if(servo_horizontal > -1)
+  {
+    servo_h.write(servo_horizontal);
+    servo_horizontal = -1;
+  }
+
+  if(servo_vertical > -1)
+  {
+    servo_v.write(servo_vertical);
+    servo_vertical = -1;
+  }
+}
+
 
 void loop()
 {
-  // car.move_forward(motor1, motor2, motor3, motor4);
-  // delay(5000);
-  // car.move_left(motor1, motor2, motor3, motor4);
-  // delay(2000);
-  // car.move_forward(motor1, motor2, motor3, motor4);
-  // delay(5000);
-  // car.move_right(motor1, motor2, motor3, motor4);
-  // delay(2000);
-  // car.stop(motor1, motor2, motor3, motor4);
-  uint8_t distance_left = ir_left.get_distance();
-  uint8_t distance_right = ir_right.get_distance();
-  uint8_t distance_front = ir_front.get_distance();
 
-  // if(distance_left < 20)
-  //   car.move_left(motor1, motor2, motor3, motor4);
-  // else if(distance_right < 20)
-  //   car.move_right(motor1, motor2, motor3, motor4);
-  // else if(distance_front < 20)
-  //   car.stop(motor1, motor2, motor3, motor4);
-  // else
-  //   car.move_forward(motor1, motor2, motor3, motor4);
+  // while (1) {
 
+  // } 
 
-  // if(i>0)
-  // {
-  //   servo1.write(180);
-  //   servo2.write(180);
-  //   i=0;
-  // }
-  // else
-  // {
-  //   servo1.write(-180);
-  //   servo2.write(-180);
-  //   i=1;
-  // }
+  // uint8_t distance_left = ir_left.get_distance();
+  // uint8_t distance_right = ir_right.get_distance();
+  // uint8_t distance_front = ir_front.get_distance();
 
-  // Serial.println("L: ");
-  // Serial.println(distance_left);
-  // Serial.println("R: ");
-  // Serial.println(distance_right);
-  // Serial.println("F: ");
-  // Serial.println(distance_front);
-
-  // Serial.println("H: ");
-  // Serial.println(servo_h.read());
-  // Serial.println("V: ");
-  // Serial.println(servo_v.read());
-
-
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-  // wait for a second
-  // delay(10);
-
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-   // wait for a second
-  // delay(10);
+  uint8_t distance_left = 0;
+  uint8_t distance_right = 0;
+  uint8_t distance_front = 0;
 
   if(Serial.available() > 0)
   {
-    buffer[buffer_counter] = Serial.read();
+    toggle_led();
+    buffer_in[buffer_counter] = Serial.read();
     // Serial.println(buffer);
-    if(buffer[buffer_counter] == ';')
+    if(buffer_in[buffer_counter] == ';')
     {
-      buffer[buffer_counter + 1] = '\0';
+      buffer_in[buffer_counter + 1] = '\0';
+      buffer_counter = 0;
       // Serial.println(buffer);
-      if(sscanf(buffer, "MF:%d,MB:%d,ML:%d,MR:%d,SH:%d,SV:%d;", &forward, &backward, &left, &right, &servo_horizontal, &servo_vertical))
-      {
-        buffer_counter = 0;
-        buffer[0] = '\0';
-        // sprintf(buffer, "MF:%d,MB:%d,ML:%d,MR:%d,SH:%d,SV:%d",forward, backward, left, right, servo_horizontal, servo_vertical);
-        // Serial.println(buffer);
-        for(uint8_t i=0;i<100;i++)
-          buffer[0] = '\0';
-
-        sprintf(buffer, "LD:%d,RD:%d,FD:%d,SH:%d,SV:%d",distance_left, distance_right, distance_front, servo_h.read(), servo_v.read());
-        Serial.println(buffer);
-      }
-      else
-      {
-        for(uint8_t i=0;i<100;i++)
-          buffer[0] = '\0';
-        buffer_counter = 0;
-        sprintf(buffer, "LD:%d,RD:%d,FD:%d,SH:%d,SV:%d",distance_left, distance_right, distance_front, servo_h.read(), servo_v.read());
-        Serial.println(buffer);
-      }
+      sscanf(buffer_in, "MF:%d,MB:%d,ML:%d,MR:%d,SH:%d,SV:%d;", &forward, &backward, &left, &right, &servo_horizontal, &servo_vertical);
+      clear_buffer(buffer_in, MAX_BUFFER_LENGTH);
     }
-    else
+    else if (buffer_counter < MAX_BUFFER_LENGTH)
       buffer_counter += 1;
+    else {
+      clear_buffer(buffer_in, MAX_BUFFER_LENGTH);
+      buffer_counter = 0;
+    }
   }
 
   if(forward > 0)
   {
-    car.move_forward(motor1, motor2, motor3, motor4);
+    car.move_forward(motor1, motor2);
     forward -= 1;
   }
 
   if(backward > 0)
   {
-    car.move_backward(motor1, motor2, motor3, motor4);
+    car.move_backward(motor1, motor2);
     backward -= 1;
   }
 
   if(left > 0)
   {
-    car.move_left(motor1, motor2, motor3, motor4);
+    car.move_left(motor1, motor2);
     left -= 1;
   }
   if(right > 0)
   {
-    car.move_right(motor1, motor2, motor3, motor4);
+    car.move_right(motor1, motor2);
     right -= 1;
   }
 
   if(forward == 0 && backward == 0 && left == 0 && right == 0)
-    car.stop(motor1, motor2, motor3, motor4);
+    car.stop(motor1, motor2);
 
-  if(servo_horizontal > 0)
+  delay(10);
+
+  if(counter > 100)
   {
-    servo_h.write(servo_horizontal);
-    servo_horizontal = 0;
+    toggle_led();
+    move_head(servo_horizontal, servo_vertical);
+    clear_buffer(buffer_out, MAX_BUFFER_LENGTH);
+    sprintf(buffer_out, "LD:%d,RD:%d,FD:%d,SH:%d,SV:%d;",distance_left, distance_right, distance_front, servo_h.read(), servo_v.read());
+    Serial.println(buffer_out);
+    counter = 0;
   }
 
-  if(servo_vertical > 0)
-  {
-    servo_v.write(servo_vertical);
-    servo_vertical = 0;
-  }
-
-  // if(counter > 100)
-  // {
-  //   sprintf(buffer, "LD:%d,RD:%d,FD:%d,SH:%d,SV:%d",distance_left, distance_right, distance_front, servo_h.read(), servo_v.read());
-  //   Serial.println(buffer);
-  //   counter = 0;
-  // }
-  // else
-  //   counter += 1;
+  counter += 1;
 }
